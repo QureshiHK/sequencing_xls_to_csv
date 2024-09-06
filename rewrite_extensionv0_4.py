f71c8df728b36f72368e52026ecf86e9636e5cb3
QureshiHK <164043943+QureshiHK@users.noreply.github.com>
2024-09-06 17:10:22 +0100

    Update rewrite_extensionv0_4.py

--- rewrite_extensionv0_4.py
+++ rewrite_extensionv0_4.py
@@ -1,25 +1,23 @@
-###### Haseeb Qureshi 2024/09/05 v0.3
+###### Haseeb Qureshi 2024/09/06 v0.41
 
 ###### changing suffix is not sufficient so switching to pandas to open xls into dataframe and save as csv
 
 import os
 import shutil
-import xlrd
-
 import pandas as pd
 
 
-asksource= input('insert source path: ')
-askdest=input('insert destination path: ')
+asksource= input('insert source path: ') #input full source directory path when prompted
+askdest=input('insert destination path: ') #input full output path
 
 print(asksource)
 
-source_directory = os.chdir(asksource)
-#current_dir = os.getcwd
+source_directory = os.chdir(asksource) #change current working directory to source directory
+
 listdirectory = os.listdir()
 
 print(listdirectory)
-#print('current directory is {} and listdir = {}'.format(current_dir,listdirectory))
+
 dest_dir = askdest
 
 for a in listdirectory:
@@ -28,25 +26,18 @@ for a in listdirectory:
         print('{} is directory'.format(a))
         continue
     prefix,extension = os.path.splitext(a)
-    #print(prefix,'+++',extension)
+    
     csvextension = '.csv'
     if extension=='.xls':
         newfilename=dest_dir+prefix+csvextension
         print('1: ', newfilename)
         
-        readxls=pd.read_excel(a,sheet_name='bmper_M1_INDEL')
-        readxls.to_csv(newfilename)
+        read_df=pd.read_excel(a,sep='\s+')
+        read_df.to_csv(newfilename)
         
-        '''        
-        shutil.copy2(a,dest_dir)
-        print('2, shutil complete')
-        
-        os.rename(newfilename,dest_dir+prefix+csvextension)
-        print('3,')
-        '''
+
         print('{} converted succesfully'.format(a))
-    #shutil.copy2(a,'dest_folder/')
-    
+  
     
 
 print('copy complete!')
